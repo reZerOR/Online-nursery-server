ee157af5b675fe2c098b888f0283f52fad07770a
reZerOR <gkraihan14k@gmail.com>
2024-09-05 21:50:52 +0600

    project initialize

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+//import router from './app/routes';
+//import globalErrorHandler from './app/middlwares/globalErrorHandler';
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// application routes
+// app.use('/api', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Save trees!');
+});
+
+// app.use(globalErrorHandler);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import config from "./app/config";
+import { Server } from "http";
+import app from "./app";
+let server: Server
+
+async function main() {
+    try {
+      await mongoose.connect(config.database_url as string);
+  
+      server = app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  main();
+
+  
+process.on('unhandledRejection', () => {
+    console.log('shutting Down');
+    if (server) {
+      server.close(() => {
+        process.exit(1);
+      });
+    }
+    process.exit(1);
+  });
+  
+  process.on('uncaughtException', () => {
+    console.log('uncaughtException is detected, shutting down ...');
+  
+    process.exit(1);
+  });
+  
